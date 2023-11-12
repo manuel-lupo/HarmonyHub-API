3463806eec4763b9f8ac5398c46ac32a559351c9
Gianluca Merendino <gmerendino@alumnos.exa.unicen.edu.ar>
2023-11-12 20:50:00 -0300

    añadido table model

---
+++ api/models/table.model.php
@@ -0,0 +1,30 @@
+<?php
+require_once './api/config.php';
+abstract class Table_model{
+    protected $db;
+    protected $table_name; //Definida por el hijo
+
+    public function __construct()
+    {
+        $this->db = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DBASE . ';charset=utf8', MYSQL_USER, MYSQL_PASS);
+    }
+
+    public function columnExists($column_name)
+    {
+        $name = $this->getTableName();
+        $query = $this->db->prepare("DESCRIBE $name");
+        $query->execute();
+        $columns = $query->fetchAll(PDO::FETCH_COLUMN);
+
+        return in_array($column_name, $columns);
+    }
+
+    public function getTableName(){
+        return $this->table_name;
+    }
+}
+models 
+
+album.model.php 
+songs.model.php 
+user.model.php 
