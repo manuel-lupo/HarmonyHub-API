789267e6f1010534aff07f11c64c976e9928f8d4
Manuel Lupo <mlupo@alumnos.exa.unicen.edu.ar>
2023-11-12 22:14:04 -0300

    Agregados require en router

--- route-api.php
+++ route-api.php
@@ -1,5 +1,9 @@
 <?php
-require_once('Router.php');
+require_once('./objects/Router.php');
+require_once('./api/config.php');
+require_once('./api/controller/album.api.controller.php');
+require_once ('./api/controller/song.api.controller.php');
+require_once "./api/controller/auth.api.controller.php";
 
 // CONSTANTES PARA RUTEO
 define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
